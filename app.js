d93574b10537cad2a33d2b6250dbf889193f278f
ddb_tribal_reggie_senal <reggie.senal@sg.tribalworldwide.com>
2022-07-27 10:04:25 +0800

    updates

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // allow lower case: true; (26)
 // allow duplicates: false,
 // set a letter as first: true
-const defargs = { length: 16, samples: 10, special_chars: false, allow_nums: true, upp_case: true, low_case: true, allow_duplicates: true, letter_first: true };
+const defargs = { length: 16, samples: 10, special_chars: true, allow_nums: true, upp_case: true, low_case: true, allow_duplicates: false, letter_first: true };
 const special_chars = [33, 35, 36, 37, 38, 64, 42, 94];
 const appargs = defargs;
 const pickRange = { min: 33, max: 122 };
